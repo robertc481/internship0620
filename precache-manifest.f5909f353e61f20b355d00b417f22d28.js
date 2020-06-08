a5037ceba2d0339a5feab35481f9eeb262b03c4b
robertc481 <49004135+robertc481@users.noreply.github.com>
2020-06-08 19:47:07 +0200

    Updates

---
+++ precache-manifest.f5909f353e61f20b355d00b417f22d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52719e575b4cd46598e807c515f426ff",
+    "url": "/internship0620/index.html"
+  },
+  {
+    "revision": "1948ddb8f0fc82c2de90",
+    "url": "/internship0620/static/css/main.7965e947.chunk.css"
+  },
+  {
+    "revision": "b4fefe202444fcb3bc6b",
+    "url": "/internship0620/static/js/2.fbb578b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/internship0620/static/js/2.fbb578b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1948ddb8f0fc82c2de90",
+    "url": "/internship0620/static/js/main.2d0111fc.chunk.js"
+  },
+  {
+    "revision": "0732e7d1b55a3a61a90a",
+    "url": "/internship0620/static/js/runtime-main.3f1c3f9b.js"
+  }
+]);
